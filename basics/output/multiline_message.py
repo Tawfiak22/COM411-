7ebdf0bb032e75838e5ffe2c4167d77a6c2343ec
repl.it user <replituser@example.com>
2021-02-26 11:13:37 +0000

    Added messages to output

---
+++ basics/output/multiline_message.py
@@ -0,0 +1,7 @@
+#Display message to standard output in multiple lines of code 
+print("System Failure Imminent!")
+print("System Roboot has been initiated...")
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine")
+print("System online")
