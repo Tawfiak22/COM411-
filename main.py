f9dcc1a5200380b057336c86d0740288d9d958c8
repl.it user <replituser@example.com>
2021-03-06 18:01:14 +0000

    upload in bot.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-print("Please enter the activity to be performed")
-activity = input()
-
-if activity == "calculate":
-  print("\nperforming calculation...")
-
+print("Towards which direction should I paint (up, down, left or right?")
+n = input()
+if(n=="up"):
+  print("I am painting in the upward direction!")
+elif(n=="down"):
+  print("I am painting in the downward direction!")
+elif(n=="left"):
+  print("I am painting in the leftward direction!")
 else:
-  print("performing activity")
-
-print("\nactivity completed!")
+  print("I am painting in the rightward direction!")
