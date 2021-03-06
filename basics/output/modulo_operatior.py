49b1a4f64a12d00a6ad27a8f1cc8b596718f959e
repl.it user <replituser@example.com>
2021-03-06 18:41:13 +0000

    upload in modulo_operartor.py

---
+++ basics/output/modulo_operatior.py
@@ -0,0 +1,4 @@
+print("Please enter a whole number.")
+n = input()
+if(n%2==0): 
+print("the number even")
--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-print("Towards which direction should I paint (up, down, left or right?")
-n = input()
-if(n=="up"):
-  print("I am painting in the upward direction!")
-elif(n=="down"):
-  print("I am painting in the downward direction!")
-elif(n=="left"):
-  print("I am painting in the leftward direction!")
+print("Please enter a whole number.")
+n=int(input())
+if(n%2==0):
+  print("The number even!")
 else:
-  print("I am painting in the rightward direction!")
+  print("The number is odd!")
